a5648bbdaccf69ed01ef912fcc67430f009bef55
RonNakata <ronald_nakata@msn.com>
2018-08-03 09:51:20 -0400

    breaking localhost trying to get env on heroku

--- keys.js
+++ keys.js
@@ -1,4 +1,20 @@
-exports.google = {
+const aws = require('aws-sdk');
+
+// let s3 = new aws.S3({
+//   accessKeyId: process.env.S3_KEY,
+//   secretAccessKey: process.env.S3_SECRET
+// });
+
+// ^^^above is example from heroku
+
+// working on localhost:
+// exports.google = {
+//   consumer_key: process.env.GOOGLE_CONSUMER_KEY,
+//   consumer_secret: process.env.GOOGLE_CONSUMER_SECRET
+// };
+
+
+exports.google = new aws.GOOGLE_CONSUMER({
   consumer_key: process.env.GOOGLE_CONSUMER_KEY,
   consumer_secret: process.env.GOOGLE_CONSUMER_SECRET
-};
+});
